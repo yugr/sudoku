63aed609117f3261d9c4d0119a4467fd5b59d6dc
Yury Gribov <tetra2005@gmail.com>
2014-09-27 00:08:28 +0400

    Support testing of compiled code.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-LOG=ghc.log
+LOG=test.log
 
-rm -f $LOG board_*.cnf *.o *.hi *.exe
+rm -f $LOG
+
+if [ "$1" = obj ]; then
+  ext=.exe
+else
+  ext=
+fi
 
 # Some hints on selecting a filling percentage:
 # * anything higher than 30% will cause current generator to frequently produce unsolvable boards
@@ -17,8 +23,7 @@ for k in 3; do
   nelts=$(($sz*$sz))
   ninits=$(($fill_ratio * $nelts / 100))
   echo "=== ${sz}x${sz} ($ninits/$nelts initialized) ==="
-  bin/GenRand $sz $ninits 0 | tee -a $LOG
-  (bin/GenRand $sz $ninits 0 | time -f 'Time: %e s' bin/Solve) 2>&1 | tee -a $LOG
+  bin/GenRand$ext $sz $ninits 0 | tee -a $LOG
+  (bin/GenRand$ext $sz $ninits 0 | time -f 'Time: %e s' bin/Solve$ext) 2>&1 | tee -a $LOG
   echo
 done
-#./build.sh
