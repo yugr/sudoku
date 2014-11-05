9018e4a5a523d792532b66dc50e08341ddc53ab9
Yury Gribov <tetra2005@gmail.com>
2014-11-06 01:22:53 +0400

    Minor testscript refactor.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-LOG=test.log
+ME=$(basename $0)
+ROOT=$(dirname $0)/..
+OBJDIR=$ROOT/bin
 
+LOG=$ROOT/test.log
 rm -f $LOG
 
 export PATH=/cygdrive/c/cygwin64/home/Yura/src/minisat-2.2.0/core:$PATH
-
-me=$(basename $0)
 
 if test "$1" = obj; then
   ext=.exe
@@ -22,16 +23,20 @@ if [ $# -eq 1 ]; then
 elif [ $# -eq 2 ]; then
   kk=$(seq $1 $2)
 else
-  echo >&2 "Syntax: $me [obj] [from] to"
+  echo >&2 "Syntax: $ME [obj] [from] to"
   exit 1
 fi
 
-# Avoid overflowing memory due to perf bugs
+# Avoid overflowing memory and killing machine
 export GHCRTS="$GHCRTS -M1G"
 
+trap 'rm -f board_*.cnf' EXIT
+
 # Some hints on selecting a filling percentage:
-# * anything higher than 30% will cause current generator to frequently produce unsolvable boards
-# * less than 20% (=17/81) is generally believed to be extra-difficult for human solver
+# * anything higher than 30% will cause current generator
+#   to frequently produce unsolvable boards
+# * less than 20% (=17/81) is generally believed
+#   to be extra-difficult for human solver
 fill_ratio=20
 
 for k in $kk; do
@@ -39,10 +44,8 @@ for k in $kk; do
   nelts=$(($sz*$sz))
   ninits=$(($fill_ratio * $nelts / 100))
   echo "=== ${sz}x${sz} ($ninits/$nelts initialized) ==="
-  bin/GenRand$ext $sz $ninits 0 | tee -a $LOG
-  (bin/GenRand$ext $sz $ninits 0 | time -f 'Time: %e s' bin/Solve$ext) 2>&1 | tee -a $LOG
+  $OBJDIR/GenRand$ext $sz $ninits 0 | tee -a $LOG
+  ($OBJDIR/GenRand$ext $sz $ninits 0 | time -f 'Time: %e s' $OBJDIR/Solve$ext) 2>&1 | tee -a $LOG
   echo
 done
-
-#rm -f board_*.cnf
 
